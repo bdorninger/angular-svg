4b175bfee5ef81ca950a92dd51a1eea4af96ee25
bdorninger <bernhard.dorninger@scch.at>
2022-04-08 12:51:24 +0200

    4 files modified

--- src/app/ssvg/ssvg.component.ts
+++ src/app/ssvg/ssvg.component.ts
@@ -11,6 +11,20 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 const rect = `<svg width="100" height="100"><rect x="0" y="0" width="100" height="100"/></svg>`;
 
+const editdark = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M5.46082 13.8117L16.316 2.95654L20.9298 7.57037L10.0746 18.4255L5.46082 13.8117ZM2.94385 20.9438L4.25469 15.0178L8.86849 19.6316L2.94385 20.9438Z" />
+</svg>`;
+
+const editlight = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M5.46082 13.8117L16.316 2.95654L20.9298 7.57037L10.0746 18.4255L5.46082 13.8117ZM2.94385 20.9438L4.25469 15.0178L8.86849 19.6316L2.94385 20.9438Z" />
+</svg>`;
+
+const svgImages = {
+  rect: rect,
+  editdark: editdark,
+  editlight: editlight,
+};
+
 @Component({
   selector: 'ssvg',
   templateUrl: './ssvg.component.html',
@@ -40,9 +54,9 @@ export class SSVGComponent implements OnInit, AfterViewInit {
   public ngAfterViewInit(): void {
     console.log('divafter', this.svgdiv);
 
-    getImage(this.key).then(html => this.svgdiv.nativeElement.outerHTML = html).catch(e => console.error('No pic',e))
-
-    
+    getImage(this.key)
+      .then((html) => (this.svgdiv.nativeElement.outerHTML = html))
+      .catch((e) => console.error('No pic', e));
   }
 
   /* NOT USED
@@ -59,5 +73,5 @@ async function getImage(key?: string): Promise<string> {
   if (key.endsWith('pic')) {
     return '<img src="https://stackblitz.com/files/angular-ivy-rbyhxs/github/bdorninger/angular-svg/master/src/assets/parser.jpg" width="100" height="100" />';
   }
-  return rect;
+  return svgImages[key] ?? rect;
 }
